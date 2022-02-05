bce6582cd64cfd2d7307953c33b93ccc7abbcbd2
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-02-05 11:11:08 -0400

    Added base PHP structure to project

---
+++ accounts.php
@@ -0,0 +1,8 @@
+<?php include('db.php') ?>
+
+<?php include('includes/header.php') ?>
+<?php include('includes/navigation.php') ?>
+
+    <h1>Accounts</h1>
+    
+<?php include('includes/footer.php') ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('db.php') ?>
+
+<?php include('includes/header.php') ?>
+<?php include('includes/navigation.php') ?>
+
+    <h1>Hello World</h1>
+    
+<?php include('includes/footer.php') ?>
